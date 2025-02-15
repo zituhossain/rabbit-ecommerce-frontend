a6ba5be147afbedd6587b6d3627e2d671dd08b3d
zituhossain5 <zituhossain@gmail.com>
2025-02-15 16:39:34 +0600

    order page

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,5 +1,122 @@
+import { useEffect, useState } from "react";
+
 const MyOrders = () => {
-  return <div>MyOrders</div>;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const mockOrders = [
+        {
+          _id: "12345",
+          createdAt: new Date(),
+          shippingAddress: {
+            city: "New York",
+            country: "United States",
+          },
+          orderItems: [
+            {
+              name: "Product 1",
+              image: "https://picsum.photos/500/500?random=1",
+            },
+          ],
+          totalPrice: 100,
+          isPaid: true,
+        },
+        {
+          _id: "67980",
+          createdAt: new Date(),
+          shippingAddress: {
+            city: "New York",
+            country: "United States",
+          },
+          orderItems: [
+            {
+              name: "Product 2",
+              image: "https://picsum.photos/500/500?random=2",
+            },
+          ],
+          totalPrice: 400,
+          isPaid: true,
+        },
+      ];
+
+      setOrders(mockOrders);
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="max-w-7xl mx-auto p-4 sm:p-6">
+      <h2 className="text-xl sm:text-2xl font-bold mb-6">My Orders</h2>
+      <div className="relative overflow-hidden shadow-md sm:rounded-lg">
+        <table className="min-w-full text-left text-gray-500">
+          <thead className="bg-gray-100 text-xs uppercase text-gray-700">
+            <tr>
+              <th className="py-2 px-4 sm:py-3">Image</th>
+              <th className="py-2 px-4 sm:py-3">Order Id</th>
+              <th className="py-2 px-4 sm:py-3">Created</th>
+              <th className="py-2 px-4 sm:py-3">Shipping Address</th>
+              <th className="py-2 px-4 sm:py-3">Items</th>
+              <th className="py-2 px-4 sm:py-3">Price</th>
+              <th className="py-2 px-4 sm:py-3">Payment Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.length > 0 ? (
+              orders.map((order) => (
+                <tr
+                  key={order._id}
+                  className="border-b hover:border-gray-50 cursor-pointer"
+                >
+                  <td className="py-2 px-2 sm:py-4 sm:px-4">
+                    <img
+                      src={order.orderItems[0].image}
+                      alt={order.orderItems[0].name}
+                      className="w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-lg"
+                    />
+                  </td>
+                  <td className="py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 whitespace-nowrap">
+                    #{order._id}
+                  </td>
+                  <td className="py-2 px-2 sm:py-4 sm:px-4">
+                    {new Date(order.createdAt).toLocaleDateString()}{" "}
+                    {new Date(order.createdAt).toLocaleTimeString()}
+                  </td>
+                  <td className="py-2 px-2 sm:py-4 sm:px-4">
+                    {order.shippingAddress
+                      ? `${order.shippingAddress.city}, ${order.shippingAddress.country}`
+                      : "N/A"}
+                  </td>
+                  <td className="py-2 px-2 sm:py-4 sm:px-4">
+                    {order.orderItems.length}
+                  </td>
+                  <td className="py-2 px-2 sm:py-4 sm:px-4">
+                    ${order.totalPrice}
+                  </td>
+                  <td className="py-2 px-2 sm:py-4 sm:px-4">
+                    <span
+                      className={`px-2 py-1 text-xs sm:text-sm font-medium rounded-full ${
+                        order.isPaid
+                          ? "bg-green-200 text-green-700"
+                          : "bg-red-200 text-red-700"
+                      }}`}
+                    >
+                      {order.isPaid ? "Paid" : "Pending"}
+                    </span>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="7" className="py-4 px-4 text-center text-gray-500">
+                  You have no orders
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
